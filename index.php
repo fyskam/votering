31815c371a9d4588c8b7fb240ca111828d2a892b
Max Isacson <nvf-info@utn.se>
2014-05-14 20:40:23 +0200

    stylade till formuläret och lade till en formateringshjälp för födelsedatumet

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 	<body>
 		<form action="post.php" method="post">
 			<table>
-				<tr><td width="100">Labbrock</td><td><input type="radio" name="vote" value="1"/></td></tr>
-				<tr><td width="100">Overall</td><td><input type="radio" name="vote" value="2"/></td></tr>
-				<tr><td width="100">Modifierad skamkappa</td><td><input type="radio" name="vote" value="3"/></td></tr>
-				<tr><td width="100">Blankt</td><td><input type="radio" name="vote" value="4"/></td></tr>
+				<tr><td width="150">Labbrock</td><td><input type="radio" name="vote" value="1"/></td></tr>
+				<tr><td width="150">Overall</td><td><input type="radio" name="vote" value="2"/></td></tr>
+				<tr><td width="150">Modifierad skamkappa</td><td><input type="radio" name="vote" value="3"/></td></tr>
+				<tr><td width="150">Blankt</td><td><input type="radio" name="vote" value="4"/></td></tr>
 				<tr><td width="100">Förnamn*</td><td><input type="text" name="fname"/></td></td>
 				<tr><td width="100">Efternamn*</td><td><input type="text" name="lname"/></td></td>
-				<tr><td width="100">Födelsedatum*</td><td><input type="text" name="nr"/></td></td>
+				<tr><td width="100">Födelsedatum*</td><td><input type="text" name="nr" style="color:#b0b0b0;" value="ÅÅÅÅ-MM-DD" onfocus="if (this.value=='ÅÅÅÅ-MM-DD') this.value=''; this.style.color='#000';"/></td></td>
 			</table>
 			<input type="submit" value="Lägg min röst"/>
 			<p>* Dessa fält är obligatoriska för att kontrollera att den som röstar finns med i medlemsregistret och alltså är röstberättigad enligt FysKams stadgar. Ingen åsiktsregistrering sker. Vid frågor kontakta Informationsansvarig på <a href="mailto:nvf-info@utn.se">nvf-info@utn.se</a>.</p>
